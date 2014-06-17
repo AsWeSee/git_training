f9cf2d980e7a9b512beeee1e4ecec25e3cf682c5
Vitalij Smolev <smolev.vitaliy@gmail.com>
2014-06-17 15:33:12 +0400

    added new file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 import random
+import massiv
 
-print(sys.argv)
+print(sys.argv, massiv.block)
 
+massiv.print_mas(1,massiv.block[0]+1)
+print(massiv.print_mas.train)
---
+++ massiv.py
@@ -0,0 +1,8 @@
+
+block = (22,32)
+
+def print_mas(lmin, lmax):
+    train = []
+    for x in range(lmin, lmax):
+        train.append(x)
+    print(train)
